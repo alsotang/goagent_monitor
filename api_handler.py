63c0da7796111ccd3eaa6d0683aa0f3d4b014b5d
Alsotang <alsotang@gmail.com>
2013-05-14 16:03:41 +0800

    Fix a api handler bug
    When retrive data form memcache, the return value is a dict. But I use it as a list wrongly.

--- api_handler.py
+++ api_handler.py
@@ -10,6 +10,7 @@ import json
 
 from fetch_config import config as fetch_config
 
+
 class ApiHandler(webapp2.RequestHandler):
     def get(self, cluster_id=None):
         cluster_attrs = fetch_config[cluster_id]
@@ -17,29 +18,24 @@ class ApiHandler(webapp2.RequestHandler):
         result = urlfetch.fetch(cluster_attrs['url'])
         appids = result.content.split('|')
 
-        mem_appids = memcache.get_multi(appids)
+        appid_dict = memcache.get_multi(appids)
 
         response_dict = {
             "available": [],
             "over_quota": [],
         }
 
-        for appid in mem_appids:
-            if bool(appid) == True:
+        for appid, val in appid_dict.iteritems():
+            if val is True:
                 response_dict['available'].append(appid)
-            elif bool(appid) == False:
+            elif val is False:
                 response_dict['over_quota'].append(appid)
 
-        response_dict['available_str'] = "今日还剩 %d GB/%d GB 流量" % (len(response_dict['available']), len(mem_appids))
+        response_dict['available_str'] = "今日还剩 %d GB/%d GB 流量" % (len(response_dict['available']), len(appids))
 
         response_json = json.dumps(response_dict, ensure_ascii=False)
 
         self.response.write(response_json)
-
-
-
-
-
 
 
 app = webapp2.WSGIApplication([
